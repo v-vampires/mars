425a4a522e7d7103ab1547ff0f00c69c3c1adf27
fitz.li <fitz.li@qunar.com>
2018-11-15 17:14:40 +0800

    修改数据库，添加注释

--- src/main/java/com/moxi/ServletInitializer.java
+++ src/main/java/com/moxi/ServletInitializer.java
@@ -3,6 +3,9 @@ package com.moxi;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.boot.web.support.SpringBootServletInitializer;
 
+/**
+ * 为了实现war包部署
+ */
 public class ServletInitializer extends SpringBootServletInitializer{
 
 	@Override
